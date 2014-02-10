a8438c2290ac5e25b803bd0b6af4ba0110e1a854
Jerome Gardiol <jerome.gardiol@gmail.com>
2014-02-11 00:58:01 +0100

    Corrected some issues with sessions

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,21 +11,28 @@ import models._
 case class Paging(current: Int, numPages: Int)
 
 object Application extends Controller {
+  
+  def user(implicit request: RequestHeader) = {
+    request.session.get("username") match {
+      case Some(name) => User.getByName(name)
+      case None => None
+    }
+  }
+  
+  def currentUri(implicit request: RequestHeader) = request.uri
 
-  var user: Option[User] = None
-  var currentUri: String = "/"
   val pageSize = 10
 
   val unauthedAction = Redirect(routes.Application.index).flashing("error" -> "Vous n'avec pas le droit de faire cela.")
 
-  def index = UserAction { implicit request =>
+  def index = Action { implicit request =>
     val forums = Forum.all() map { forum =>
       (forum, Forum.getInfo(forum.id))
     }
     Ok(views.html.index(forums, user))
   }
 
-  def forum(id: Long, page: Int = 0) = UserAction { implicit request =>
+  def forum(id: Long, page: Int = 0) = Action { implicit request =>
     Forum.getById(id) match {
       case Some(forum) => {
         val threads = Forum.getThreads(id, page, pageSize).map(t => (t, Thread.getThreadInfo(t.id)))
@@ -36,7 +43,7 @@ object Application extends Controller {
     }
   }
 
-  def thread(id: Long, page: Int = 0) = UserAction { implicit request =>
+  def thread(id: Long, page: Int = 0) = Action { implicit request =>
     Thread.getById(id) match {
       case Some(thread) => {
         val forum = Forum.getById(thread.forumId).get
@@ -59,7 +66,7 @@ object Application extends Controller {
         Redirect(currentUri).flashing("error" -> "Vous devez saisir un nom d'utilisateur et un mot de passe.")
       },
       userData => {
-        user = User.authenticate(userData._1, userData._2)
+        val user = User.authenticate(userData._1, userData._2)
         user match {
           case Some(user) => Redirect(currentUri).withSession("name" -> user.name)
           case None => Redirect(currentUri).flashing("error" -> "Mauvais nom d'utilisateur ou mot de passe")
@@ -85,7 +92,7 @@ object Application extends Controller {
         if (data._2 != data._3)
           Redirect(currentUri).flashing("error" -> "Les mots de passe ne correspondent pas.")
         else {
-          user = User.create(data._1, data._2, data._4, "-")
+          val user = User.create(data._1, data._2, data._4, "-")
           user match {
             case Some(user) => {
               Redirect(currentUri).withSession("name" -> user.name).flashing("success" -> "Compte créé.")
@@ -96,12 +103,11 @@ object Application extends Controller {
       })
   }
 
-  def logout = Action { request =>
-    user = None
+  def logout = Action { implicit request =>
     Redirect(currentUri).withNewSession
   }
 
-  def admin = UserAction { implicit request =>
+  def admin = Action { implicit request =>
     user match {
       case Some(user) => {
         user.role match {
@@ -113,7 +119,7 @@ object Application extends Controller {
     }
   }
 
-  def forumadmin = UserAction { implicit request =>
+  def forumadmin = Action { implicit request =>
     user match {
       case Some(user) => {
         user.role match {
@@ -245,17 +251,5 @@ object Application extends Controller {
       }
       case None => Unauthorized("Vous devez être connecté!")
     }
-  }
-}
-
-object UserAction extends ActionBuilder[Request] {
-  def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[SimpleResult]) = {
-    request.session.get("username") match {
-      case Some(username) => Application.user = User.getByName(username)
-      case None =>
-    }
-
-    Application.currentUri = request.uri
-    block(request)
   }
 }
